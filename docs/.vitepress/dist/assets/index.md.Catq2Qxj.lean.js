3be661dbe50a42f4931105845fe34f9a6280369c
arobot001 <1831161001@qq.com>
2026-02-05 19:04:00 +0800

    feat: 添加 daily/index.md 简介页面
    - 添加简单的介绍页面，包含如何查看日报的说明
    - 侧边栏仍然显示日报列表
    - 发布脚本跳过 index.md，不将其添加到侧边栏

--- docs/.vitepress/dist/assets/index.md.Catq2Qxj.lean.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/framework.CvG8q2Vs.js";import{__pageData as o,default as p}from"./README.md.BIpxZDOc.js";export{o as __pageData,p as default};
---
+++ docs/.vitepress/dist/assets/index.md.Cbxk6yju.lean.js
@@ -0,0 +1,1 @@
+import"./chunks/framework.DGvXf2_7.js";import{__pageData as o,default as p}from"./README.md.CG1IlkmY.js";export{o as __pageData,p as default};
